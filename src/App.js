6612e74006cd63f5c21d16a9c503558ddfb93334
miraclechamberlain12@gmail.com <miraclechamberlain12@gmail.com>
2020-07-31 13:24:09 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Homepage from './homepage.component.jsx'
+
+
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+      <Homepage />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/homepage.component.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import './styles.css';
+
+const Homepage = ()=>(
+  <div className='homepage'>
+  <div className='directory-menu'>
+  <div className='menu-item'>
+  <div className='content'>
+<h1 className='title'>HATS</h1>
+<h4 className='subtitle'>Shop Now</h4>
+  </div>
+  </div>
+
+  <div className='menu-item'>
+  <div className='content'>
+<h1 className='title'>JACKECTS</h1>
+<h4 className='subtitle'>Shop Now</h4>
+  </div>
+  </div>
+
+  <div className='menu-item'>
+  <div className='content'>
+<h1 className='title'>SNEAKERS</h1>
+<h4 className='subtitle'>Shop Now</h4>
+  </div>
+  </div>
+
+  <div className='menu-item'>
+  <div className='content'>
+<h1 className='title'>WOMEN</h1>
+<h4 className='subtitle'>Shop Now</h4>
+  </div>
+  </div>
+
+  <div className='menu-item'>
+  <div className='content'>
+<h1 className='title'>MENS</h1>
+<h4 className='subtitle'>Shop Now</h4>
+  </div>
+  </div>
+
+
+  </div>
+  </div>
+);
+
+export default Homepage;
